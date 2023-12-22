e4469e4e7da6af1bb7767cd04f919f84d76390d3
Urho Laukkarinen <urho.laukkarinen@gmail.com>
2023-12-22 22:04:03 +0200

    Fixed demo not loading

--- demo/build_web.sh
+++ demo/build_web.sh
@@ -1,2 +1,2 @@
-trunk build --release --filehash false --dist ../docs
+trunk build --config Trunk.toml --release
 wasm-opt -Oz ../docs/egui-gizmo-demo_bg.wasm --output ../docs/egui-gizmo-demo_bg.wasm
