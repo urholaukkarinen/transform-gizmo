c7a3e272b6387a38c0e8bef721ea3bccd9feff95
ActuallyHappening <105958073+ActuallyHappening@users.noreply.github.com>
2024-06-09 19:51:55 +1000

    add: bevy_minimal.rs example

---
+++ crates/transform-gizmo-bevy/examples/bevy_minimal.rs
@@ -0,0 +1,46 @@
+use bevy::prelude::*;
+use transform_gizmo_bevy::*;
+
+fn main() {
+    App::new()
+        .add_plugins((DefaultPlugins, TransformGizmoPlugin))
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut meshes: ResMut<Assets<Mesh>>,
+) {
+    // camera
+    commands.spawn((
+        Camera3dBundle {
+            transform: Transform::from_translation(Vec3::new(1.0, 3.0, -5.0))
+                .looking_at(Vec3::ZERO, Vec3::Y),
+            ..default()
+        },
+        GizmoCamera,
+    ));
+
+    // cube
+    commands.spawn((
+        PbrBundle {
+            mesh: meshes.add(Cuboid::new(1.0, 1.0, 1.0)),
+            material: materials.add(Color::GREEN),
+            transform: Transform::from_translation(Vec3::new(0.0, 0.0, 0.0)),
+            ..default()
+        },
+        GizmoTarget::default(),
+    ));
+
+    // light
+    commands.spawn(PointLightBundle {
+        point_light: PointLight {
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..default()
+    });
+}
